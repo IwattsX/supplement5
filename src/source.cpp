ea690606cde4c2de1a9a9b8ea46c1394719fbab3
= <Isaac.s.watts@gmail.com>
2024-11-24 01:44:06 -0500

    Documentation: added documentation for functions get_sqrt number_cases is_divisible_list

--- src/source.cpp
+++ src/source.cpp
@@ -2,6 +2,14 @@
 #include <stdexcept>
 #include "number_utils.h"
 
+/**
+ * @brief Get the sqrt of a number
+ * 
+ * @param num
+ * @return double the sqrt of the number
+ * 
+ * @throw std::invalid_argument if the num is negative 
+ */
 double get_sqrt(const double num){
     if(num < 0){
         throw std::invalid_argument("recieved negative number");
@@ -9,6 +17,23 @@ double get_sqrt(const double num){
     return sqrt(num);
 }
 
+/**
+ * @brief 
+ * Seeds and gets a random number
+ * if odd it multiplies it by 2
+ * 
+ * if divisible by 3 it divides it by 3
+ * 
+ * if divisible by 4 it multiplies it by 4
+ * 
+ * if num after all this is greater than 4, throws an error
+ * else returns that number
+ * 
+ * @param test_num used for testing
+ * @return int the number after all these if statements are applied
+ * 
+ * @throws std::invalid_argument if after applying the operations the number is greater than 4
+ */
 int number_cases(int* test_num = nullptr){
     srand(time(0));
     int number_test = rand();
@@ -38,6 +63,14 @@ int number_cases(int* test_num = nullptr){
     return number_test;
 }
 
+/**
+ * @brief Gets a divisibility list by the num from 1 to 10
+ * 
+ * @param num the number to check for divisibility by for [1, 10]
+ * @return std::vector<int> a list that has the numbers that are divisible by num from 1 to 10 (set notation)  -> {xE[1, 10] | x mod(num) = 0}
+ * 
+ * @throw std::invalid_argument for if the num is 0 
+ */
 std::vector<int> is_divisible_list(const int num) {
     if (num == 0) {
         throw std::invalid_argument("Division by zero is not allowed");
